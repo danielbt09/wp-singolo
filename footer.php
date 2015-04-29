3c9688c27be4e0de2f9a68f99bd387f608aabae0
daniel sandu <daniel.sandu@info.uaic.ro>
2015-04-29 11:57:55 +0300

    Added footer section.

--- footer.php
+++ footer.php
@@ -1,5 +1,16 @@
-
-
+<footer class="footer">
+		<div class="container">
+			<div class="copyright">
+				<span>&copy; Copyright 2015 </span> by PSDchat.com
+			</div><!--
+		 --><div class="social-media">
+				<a href="#"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-google-plus"></i></a>
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-linkedin"></i></a>
+			</div>
+		</div>
+	</footer>
 
 	<?php wp_footer(); ?>
 </body>	
